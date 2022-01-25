1200f5db790683e6a4eb8746e9fc813ec6f7ca4e
ambushay <60261499+ambushay@users.noreply.github.com>
2022-01-25 20:53:39 +0330

    new messaging system

---
+++ src/messaging/Chatroom.java
@@ -0,0 +1,61 @@
+package messaging;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import UserManagment.User;
+
+public class Chatroom {
+	private static List<Chatroom> allchatrooms = new ArrayList<>(); 
+	private User user1;
+	private User user2;
+	private List<Message> messages = new ArrayList<>();
+	private String name;
+	
+	public Chatroom(User sender, User receiver) {
+		user1 = sender;
+		user2 = receiver;
+		name = user1.getUsername() + " and " + user2.getUsername() + " chatroom";
+	}
+	
+	public void setSender(User s) {
+		this.user1 = s;
+	}
+	
+	public User getSender() {
+		return this.user1;
+	}
+	
+	public void setReceiver(User s) {
+		this.user2 = s;
+	}
+	
+	public User getReceiver() {
+		return this.user2;
+	}
+	
+	public void setName(String str) {
+		this.name = str;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setMessages(List<Message> msgl) {
+		this.messages = msgl;
+	}
+	
+	public List<Message> getMessages(){
+		return this.messages;
+	}
+	
+	public static void setAllchats(List<Chatroom> chts) {
+		Chatroom.allchatrooms = chts;
+	}
+	
+	public static List<Chatroom> getAllchats(){
+		return Chatroom.allchatrooms;
+	}
+	
+}
